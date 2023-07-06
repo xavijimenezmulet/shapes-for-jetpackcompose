d14d21bb1b5f40cf44ec16d5d5819962a05e0337
Xavier Jimenez <xavijimenezmulet@macaqueconsulting.com>
2023-07-06 22:18:29 +0200

    Added star shape

--- sample/src/main/java/com/xavijimenezmulet/shapesforjetpackcompose/MainActivity.kt
+++ sample/src/main/java/com/xavijimenezmulet/shapesforjetpackcompose/MainActivity.kt
@@ -3,13 +3,28 @@ package com.xavijimenezmulet.shapesforjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.xavijimenezmulet.shapes.StarShape
 import com.xavijimenezmulet.shapesforjetpackcompose.ui.theme.ShapesForJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Grid()
                 }
             }
         }
@@ -30,17 +45,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Grid() {
+    val gridItems = listOf(
+        Shapes.Star
     )
+    LazyVerticalGrid(
+        modifier = Modifier.fillMaxSize(),
+        columns = GridCells.Fixed(2)
+    ) {
+        items(gridItems.size) { item ->
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .size(100.dp)
+                        .clip(gridItems[item].shape)
+                        .background(Color.Red),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("Star", fontWeight = FontWeight.Bold)
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GridPreview() {
     ShapesForJetpackComposeTheme {
-        Greeting("Android")
+        Grid()
     }
+}
+
+sealed class Shapes(val shape: Shape) {
+    object Star : Shapes(shape = StarShape(5))
 }
